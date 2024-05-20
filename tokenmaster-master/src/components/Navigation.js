865a012cd5cdc1e7377ff1deb619a518925a4c8f
monish <srs2kin04@gmail.com>
2024-05-20 12:22:18 +0530

    Initial commit

---
+++ tokenmaster-master/src/components/Navigation.js
@@ -0,0 +1,43 @@
+import { ethers } from 'ethers'
+
+const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  return (
+    <nav>
+      <div className='nav__brand'>
+        <h1>Bookmyticket</h1>
+
+        <input className='nav__search' type="text" placeholder='Find millions of experiences' />
+
+        <ul className='nav__links'>
+          <li><a href="/">help</a></li>
+         
+        </ul>
+      </div>
+
+      {account ? (
+        <button
+          type="button"
+          className='nav__connect'
+        >
+          {account.slice(0, 6) + '...' + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button
+          type="button"
+          className='nav__connect'
+          onClick={connectHandler}
+        >
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+}
+
+export default Navigation;
